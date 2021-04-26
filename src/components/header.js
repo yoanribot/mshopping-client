42a60286960414d5ceb45279ef97fe9378f7eb0c
Yoan <yoanribot@gmail.com>
2021-04-26 18:46:42 +0200

    Add user menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import UserMenu from './user-menu';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,10 +41,10 @@ export default function Header() {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+            <SupervisorAccountIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            iTeam
           </Typography>
           {!isAuthenticated && (
 						<Button
@@ -60,12 +61,11 @@ export default function Header() {
 						>
 							Login
 						</Button>)
-						: (<Button
-							color="inherit"
-							onClick={_logout}
-						>
-							Logout
-						</Button>)
+						: (
+							<UserMenu
+								onLogout={_logout}
+							/>
+						)
 					}
         </Toolbar>
       </AppBar>
---
+++ src/components/user-menu.js
@@ -0,0 +1,71 @@
+import React, { memo, useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Context as UserContext } from '../context/user';
+import { useHistory } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles((theme) => ({
+  userLabel: {
+    marginLeft: 10,
+    color: 'white',
+  },
+}));
+
+const UserMenu = memo(({ onLogout }) => {
+  const history = useHistory();
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { currentUser } = useContext(UserContext);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const goToProfile = () => {
+    history.push('/profile');
+    setAnchorEl(null);
+  };
+
+  const _onLogout = () => {
+    setAnchorEl(null);
+    onLogout();
+  };
+
+  const handleClose = () => setAnchorEl(null);
+
+  return (
+    <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        <Avatar src="/broken-image.jpg" />
+        <p className={classes.userLabel}> {currentUser.name} </p>
+      </Button>
+      <Menu
+        id="simple-menu"
+        className={classes.userMenuWrapper}
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={goToProfile}>Profile</MenuItem>
+        <MenuItem onClick={_onLogout}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+});
+
+UserMenu.propTypes = {
+  onLogout: PropTypes.func,
+};
+
+UserMenu.defaultProps = {
+
+};
+
+export default UserMenu;
